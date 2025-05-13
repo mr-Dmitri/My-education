0654cc6e7a1d4545049a00236ee9714ec6a8515d
Дмитрий <mrdmitriy@tut.by>
2025-05-13 17:52:02 +0300

    Главный файл

---
+++ BELHARD/DataScience/BaseLevel/HomeWork/№02/main.py
@@ -0,0 +1,6 @@
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.preprocessing import LabelEncoder
+from loader import load_data
+from sklearn.impute import SimpleImputer
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
