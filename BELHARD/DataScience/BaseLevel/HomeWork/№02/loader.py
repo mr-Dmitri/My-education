100e85aee47cf00b28d1b93147fe0e032be06833
Дмитрий <mrdmitriy@tut.by>
2025-05-17 12:45:58 +0300

    Добавил загрузку датасета из kaggle

--- BELHARD/DataScience/BaseLevel/HomeWork/№02/loader.py
+++ BELHARD/DataScience/BaseLevel/HomeWork/№02/loader.py
@@ -1,9 +1,37 @@
+import os
+from kaggle.api.kaggle_api_extended import KaggleApi
+
 import pandas as pd
 
-def load_data(file_path):
+def load_data_csv(file_path):
     """
     Загрузка данных из CSV файла.
     :param file_path: Путь к CSV файлу.
     :return: DataFrame с загруженными данными.
     """
-    return pd.read_csv(file_path)
+    try:
+        return pd.read_csv(file_path)
+    except:
+        return None
+
+def load_kaggle_data_set(dataset):
+    """
+    Загрузка датасета из kaggle.
+
+    Важно: Необходимо предварительно получить API Token.
+    Переместите файл kaggle.json в папку ~/.kaggle/ (для Linux и macOS)
+    или в C:/Users/<Ваше_имя_пользователя>/.kaggle/ (для Windows).
+    Если папка .kaggle не существует, создайте ее.
+    :param dataset:
+    :return:
+    """
+
+    api = KaggleApi()
+    api.authenticate()
+    api.dataset_download_files(dataset=dataset,
+                               path='./data',
+                               force=True,
+                               unzip=True)              # Загрузка и распаковка
+
+
+
