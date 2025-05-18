546cd1095c2149096eacfd5ed3bdaffc6353736c
Дмитрий <mrdmitriy@tut.by>
2025-05-18 22:00:55 +0300

    Модели для таблиц в БД

--- BELHARD/DataScience/BaseLevel/HomeWork/№03/models/models_tables.py
+++ BELHARD/DataScience/BaseLevel/HomeWork/№03/models/models_tables.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, String, Float, Date, DateTime
+from sqlalchemy import Column, Integer, String, Date, Float
 
 Base = declarative_base()
 
@@ -7,56 +7,56 @@ Base = declarative_base()
 class Gender(Base):
     __tablename__ = 'genders'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    gender = Column(String)
+    Gender = Column(String)
 
 class BloodType(Base):
     __tablename__ = 'blood_types'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    blood_type = Column(String)
+    BloodType = Column(String)
 
 class MedicalCondition(Base):
     __tablename__ = 'medical_conditions'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    medical_condition = Column(String)
+    MedicalCondition = Column(String)
 
 class Doctor(Base):
     __tablename__ = 'doctors'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    doctor_name = Column(String)
+    Doctor = Column(String, unique=True)
 
 class Hospital(Base):
     __tablename__ = 'hospitals'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    hospital_name = Column(String)
+    Hospital = Column(String)
 
 class InsuranceProvider(Base):
     __tablename__ = 'insurance_providers'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    insurance_provider = Column(String)
+    InsuranceProvider = Column(String)
 
 class AdmissionType(Base):
     __tablename__ = 'admission_types'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    admission_type = Column(String)
+    AdmissionType = Column(String)
 
 class Medication(Base):
     __tablename__ = 'medications'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    medication = Column(String)
+    Medication = Column(String)
 
 class TestResults(Base):
     __tablename__ = 'test_results'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    test_result = Column(String)
+    TestResults = Column(String)
 
 
 class Patient(Base):
     __tablename__ = 'patients'
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     Name = Column(String)
     Age = Column(Integer)
-    DateAdmission = Column(Date)
+    DateOfAdmission = Column(Date)
     BillingAmount = Column(Float)
     RoomNumber = Column(Integer)
     DischargeDate = Column(Date)
-    LengthStay = Column(Integer)
+    LengthOfStay = Column(Integer)
