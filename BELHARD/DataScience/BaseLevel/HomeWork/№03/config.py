2a870001abedecac7603cd388a270f50a8a6a769
Дмитрий <mrdmitriy@tut.by>
2025-05-18 19:04:08 +0300

    Настройки

--- BELHARD/DataScience/BaseLevel/HomeWork/№03/config.py
+++ BELHARD/DataScience/BaseLevel/HomeWork/№03/config.py
@@ -5,4 +5,8 @@ db_dir = 'database'
 db_name = 'dataset.db'
 db_path = os.path.join(root_dir, db_dir, db_name)
 db_url = f'sqlite:///{db_path}'
-print(db_path)
+
+dataset_dir = 'dataset'
+# dataset_name = 'Covid Data.csv'
+dataset_name = 'modified_healthcare_dataset.csv'
+dataset_path = os.path.join(root_dir, dataset_dir, dataset_name)
