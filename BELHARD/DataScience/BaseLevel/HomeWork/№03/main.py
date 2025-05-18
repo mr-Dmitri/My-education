3f27f61adc6529795e34f190b2f3e90410ea73cc
Дмитрий <mrdmitriy@tut.by>
2025-05-18 22:07:52 +0300

    Перенес действия в функции

--- BELHARD/DataScience/BaseLevel/HomeWork/№03/main.py
+++ BELHARD/DataScience/BaseLevel/HomeWork/№03/main.py
@@ -5,8 +5,7 @@ from loader import load_data_csv, load_kaggle_data_set
 from config import dataset_path
 from datetime import date
 from models.models_tables import Patient
-from d_base import create_tables, add_patient, add_all_from_dataset_to_table
-
+from d_base import create_tables, fill_db_tabes
 
 kaggle_data_set = 'eduardolicea/healthcare-dataset'
 
@@ -42,34 +41,12 @@ if __name__ == '__main__':
                            },
                   inplace=True
         )                                                                   # Переименовать столбцы лоя дальнейшей работы
+
         df.info()                                                           # Информация о датафрейме (датасете)
-
         db_cerate()                                                         # Создать БД
-
         create_tables()                                                     # Создать таблицы в БД
+        fill_db_tabes(df)                                                   # Заполнить таблицы в БД соответствующими данными из датасета (датафрейма)
 
-        add_all_from_dataset_to_table(df,'genders',['Gender'])
-        add_all_from_dataset_to_table(df,'blood_types',['BloodType'])
-        add_all_from_dataset_to_table(df,'medical_conditions',['MedicalCondition'])
-        add_all_from_dataset_to_table(df,'doctors',['Doctor'])
-        add_all_from_dataset_to_table(df,'hospitals',['Hospital'])
-        add_all_from_dataset_to_table(df,'insurance_providers',['InsuranceProvider'])
-        add_all_from_dataset_to_table(df,'admission_types',['AdmissionType'])
-        add_all_from_dataset_to_table(df,'medications',['Medication'])
-        add_all_from_dataset_to_table(df,'test_results',['TestResults'])
-        add_all_from_dataset_to_table(df,'patients')
-
-
-        # patient = Patient(Name='Bobby Jackson',
-        #                   Age=19,
-        #                   DateAdmission=date.fromisoformat('2024-01-31'),
-        #                   BillingAmount = 2212.272701009033,
-        #                   RoomNumber = 1,
-        #                   DischargeDate = date.fromisoformat('2024-02-07'),
-        #                   LengthStay=7,
-        #                   )
-
-        # add_patient(patient)
 
     except Exception as e:
         print(f'При выполнении произошла ошибка: {e}')
