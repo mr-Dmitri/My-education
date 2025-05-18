179a24e61dfd3638fd03ba7e48231717a598648f
Дмитрий <mrdmitriy@tut.by>
2025-05-18 22:06:22 +0300

    Добавлены функкции: create_tables, add_patient, add_all_from_dataset_to_table, fill_db_tabes

--- BELHARD/DataScience/BaseLevel/HomeWork/№03/d_base.py
+++ BELHARD/DataScience/BaseLevel/HomeWork/№03/d_base.py
@@ -1,12 +1,13 @@
 import os
 import json
+import pandas as pd
 
 from config import db_url
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, inspect, text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from models.models_tables import Patient, Base
 
-Base = declarative_base()                                   # Базовый класс для моделей
 
 engine = create_engine(db_url, echo=False)
 Session = sessionmaker(bind=engine)
@@ -21,70 +22,47 @@ def db_cerate():
     except Exception as e:
         raise (f'Ошибка создания базы данных: {e}')
 
+def create_tables():
+    Base.metadata.create_all(engine)
 
-# def import_from_json(file_path):
-#     with open(file_path, 'r', encoding='utf-8') as file:
-#         data = json.load(file)
-#     return data
-#
-# def import_rates_from_json_file(file_path):
-#     data = import_from_json(file_path)
-#
-#     engine = create_engine(database_url)
-#     with Session(engine) as session:
-#         for row in data:
-#             r = Rate(Cur_Abbreviation = row.get('Cur_Abbreviation'),
-#                      Cur_ID	 = row.get('Cur_ID'),
-#                      Cur_Name = row.get('Cur_Name'),
-#                      Cur_OfficialRate = row.get('Cur_OfficialRate'),
-#                      Cur_Scale = row.get('Cur_Scale'),
-#                      Date = row.get('Date'))
-#             session.add(r)
-#         try:
-#             session.commit()
-#         except:
-#             pass
-#
-#
-# def import_rates_from_json_files(files_list):
-#     for file_name in files_list:
-#         import_rates_from_json_file(file_name)
-#
-#
-# def import_currencs_from_json(file_path):
-#     data = import_from_json(file_path)
-#
-#     base_name = 'Exrates.db'
-#     base_path = os.path.abspath(os.path.dirname(__file__))
-#     ful_base_name = f'{base_path}/{base_name}'
-#     database_url = f'sqlite:///{ful_base_name}'
-#
-#     engine = create_engine(database_url)
-#     with Session(engine) as session:
-#         for row in data:
-#             c = Currency(Cur_ID = row.get('Cur_ID'),
-#                          Cur_ParentID = row.get('Cur_ParentID'),
-#                          Cur_Code = row.get('Cur_Code'),
-#                          Cur_Abbreviation = row.get('Cur_Abbreviation'),
-#                          Cur_Name = row.get('Cur_Name'),
-#                          Cur_Name_Bel = row.get('Cur_Name_Bel'),
-#                          Cur_Name_Eng = row.get('Cur_Name_Eng'),
-#                          Cur_QuotName = row.get('Cur_QuotName'),
-#                          Cur_QuotName_Bel = row.get('Cur_QuotName_Bel'),
-#                          Cur_QuotName_Eng = row.get('Cur_QuotName_Eng'),
-#                          Cur_NameMulti = row.get('Cur_NameMulti'),
-#                          Cur_Name_BelMulti = row.get('Cur_Name_BelMulti'),
-#                          Cur_Name_EngMulti = row.get('Cur_Name_EngMulti'),
-#                          Cur_Scale = row.get('Cur_Scale'),
-#                          Cur_Periodicity = row.get('Cur_Periodicity'),
-#                          Cur_DateStart = row.get('Cur_PeriodiCur_DateStartcity'),
-#                          Cur_DateEnd = row.get('Cur_DateEnd'),
-#                         )
-#             try:
-#                 session.add(c)
-#             except:
-#                 pass
-#
-#         session.commit()
-#
+def add_patient(patient : Patient):
+    print(patient)
+    with Session() as session:
+        try:
+            session.add(patient)
+            session.commit()
+        except Exception as e:
+            raise (e)
+
+def add_all_from_dataset_to_table(df: pd.DataFrame,
+                                  table_name : str,
+                                  unique_columns : list = None):
+    with Session() as session:
+        truncate_table = text(f'DELETE FROM {table_name}')
+        session.execute(truncate_table)
+        session.commit()
+
+    inspector = inspect(engine)
+
+    columns = [col['name'] for col in inspector.get_columns(table_name)
+               if col['name']  != 'id']                                                     # Получить список столбцов из таблицы 'patients', исключая id, т.к. он автоинкриментный в таблице
+    if unique_columns:                                                                      # Если передан лист имен столбцов в которых значения д.б. уникальными
+        df_filtered = df[columns].drop_duplicates(unique_columns)                           # Фильтр DataFrame, оставляя только те столбцы, которые есть в таблице, и дополнительный фильтр по уникальным
+    else:
+        df_filtered = df[columns]                                                           # Фильтр DataFrame, оставляя только те столбцы, которые есть в таблице
+
+    df_filtered.to_sql(table_name, con=engine, if_exists='append', index=False)             # Запись DataFrame в таблицу
+
+
+def fill_db_tabes(df: pd.DataFrame):
+    add_all_from_dataset_to_table(df, 'genders', ['Gender'])
+    add_all_from_dataset_to_table(df, 'blood_types', ['BloodType'])
+    add_all_from_dataset_to_table(df, 'medical_conditions', ['MedicalCondition'])
+    add_all_from_dataset_to_table(df, 'doctors', ['Doctor'])
+    add_all_from_dataset_to_table(df, 'hospitals', ['Hospital'])
+    add_all_from_dataset_to_table(df, 'insurance_providers', ['InsuranceProvider'])
+    add_all_from_dataset_to_table(df, 'admission_types', ['AdmissionType'])
+    add_all_from_dataset_to_table(df, 'medications', ['Medication'])
+    add_all_from_dataset_to_table(df, 'test_results', ['TestResults'])
+    add_all_from_dataset_to_table(df, 'patients')
 
